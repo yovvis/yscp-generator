899a77eb9f682d4d3871519627c5df8863734259
tyangjian <meettyj.@163.com>
2023-11-23 16:25:13 +0800

    perf 交互式命令

--- yscp-generator-basic/src/main/java/com/yovvis/cli/example/Login.java
+++ yscp-generator-basic/src/main/java/com/yovvis/cli/example/Login.java
@@ -1,5 +1,8 @@
 package com.yovvis.cli.example;
 
+import picocli.CommandLine;
+import picocli.CommandLine.Option;
+
 import java.util.concurrent.Callable;
 
 /**
@@ -8,13 +11,29 @@ import java.util.concurrent.Callable;
 public class Login implements Callable<Integer> {
 
 
+    @Option(names = {"-u", "--username"}, description = "User Name")
     String username;
+    @Option(names = {"-p", "--password"}, description = "Password", echo = false, arity = "0..1", prompt = "请输入密码：", interactive = true)
     String password;
+    @Option(names = {"-cp", "--checkpassword"}, description = "Check Password", arity = "0..1", echo = true, prompt = "请确认密码：", interactive = true)
     String checkPassword;
 
     @Override
     public Integer call() throws Exception {
-        System.out.println();
+        System.out.println("password=" + password);
+        System.out.println("checkPassword=" + checkPassword);
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        // 弱交互加上arity="0..1"
+        // 利用反射强交互
+        int exitCode = new CommandLine(new Login()).execute("-u", "yovvis", "-p", "xxx", "-cp");
+        System.exit(exitCode);
+    }
+
+    // todo 根据反射获取强制交互
+    public static String[] getDecison(String[] args) {
         return null;
     }
 }
