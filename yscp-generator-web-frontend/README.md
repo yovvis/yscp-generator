04debff47265bf80bb1081256aea18eaf3fe8264
tyangjian <meettyj.@163.com>
2024-02-01 19:21:40 +0800

    del 清楚web，重新提交

--- yscp-generator-web-frontend/README.md
+++
@@ -1,5 +0,0 @@
-# Yovvis前端万用模板
-
-> 基于 Ant Design Pro 二次开发，更简单易用
-
-
--- yscp-generator-web-frontend/src/constants/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * 本地后端地址
- */
-export const BACKEND_HOST_LOCAL = "http://localhost:8089/api";
-
-/**
- * 线上后端地址
- */
-export const BACKEND_HOST_PROD = "https://www.yovvis.top";
